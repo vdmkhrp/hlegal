aa7303d089f5975a7b7d07134879cf3686405630
vdmkhrp <vdmkhrp@gmail.com>
2024-08-02 21:06:44 +0300

    created  menu for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,15 @@
 // HidePopup.addEventListener("click", () => {
 //   document.body.style.overflow = 'auto';
 // });
+
+const burgerBtn = document.querySelector(".burger-btn");
+const menu = document.querySelector(".header__nav");
+const closeBtn = document.querySelector(".close-btn");
+
+burgerBtn.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("active");
+});

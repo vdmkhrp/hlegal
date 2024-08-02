c6e71406b6f9d300efcf73bbed4a63659cde69ca
vdmkhrp <vdmkhrp@gmail.com>
2024-08-02 17:58:42 +0300

    created new page and popup

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+// const ShowPopup = document.querySelector(".service-entry__block-button");
+// const HidePopup = document.querySelector(".popup__close-btn");
+
+// ShowPopup.addEventListener("click", () => {
+//   document.body.style.overflow = 'hidden';
+// });
+
+// HidePopup.addEventListener("click", () => {
+//   document.body.style.overflow = 'auto';
+// });

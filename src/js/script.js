6018b96b711880a9d3c31c459d066a9e14dadd33
vdmkhrp <vdmkhrp@gmail.com>
2024-08-03 17:40:57 +0300

    parcel fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,3 @@
-// const ShowPopup = document.querySelector(".service-entry__block-button");
-// const HidePopup = document.querySelector(".popup__close-btn");
-
-// ShowPopup.addEventListener("click", () => {
-//   document.body.style.overflow = 'hidden';
-// });
-
-// HidePopup.addEventListener("click", () => {
-//   document.body.style.overflow = 'auto';
-// });
-
 const burgerBtn = document.querySelector(".burger-btn");
 const menu = document.querySelector(".header__nav");
 const closeBtn = document.querySelector(".close-btn");

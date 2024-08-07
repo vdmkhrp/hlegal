891d44f9125686d45a48f6df36ea7e02352e4e25
vdmkhrp <vdmkhrp@gmail.com>
2024-08-07 19:47:06 +0300

    styles for the team page

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ const burgerBtn = document.querySelector(".burger-btn");
 const menu = document.querySelector(".header__nav");
 const closeBtn = document.querySelector(".close-btn");
 const tabsItems = document.querySelectorAll(".tabs__item");
+const swiperPag = document.querySelectorAll(".swiper-pagination__btn");
+const cardItemLinks = document.querySelectorAll(".teams__card-item-link");
 
 burgerBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
@@ -17,3 +19,17 @@ tabsItems.forEach((item) => {
     item.classList.add("tabs__item--active");
   });
 });
+
+swiperPag.forEach((item) => {
+  item.addEventListener("click", () => {
+    swiperPag.forEach((el) => el.classList.remove("swiper-pagination__btn--active"));
+    item.classList.add("swiper-pagination__btn--active");
+  });
+});
+
+// cardItemLinks.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     cardItemLinks.forEach((el) => el.classList.remove("teams__card-item-link--active"));
+//     item.classList.add("teams__card-item-link--active");
+//   });
+// });

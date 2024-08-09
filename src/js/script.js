314033520a026802209ab6bb94660396e93941b4
vdmkhrp <vdmkhrp@gmail.com>
2024-08-09 20:25:03 +0300

    connected and configured the slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,121 @@
 import Glide from "@glidejs/glide";
 
-new Glide(".glide", {
+const defaultCarouselSettings = {
   type: "carousel",
-}).mount();
+  classes: {
+    nav: {
+      active: "swiper-pagination__btn--active",
+    },
+  },
+};
 
-// const burgerBtn = document.querySelector(".burger-btn");
-// const menu = document.querySelector(".header__nav");
-// const closeBtn = document.querySelector(".close-btn");
-// const tabsItems = document.querySelectorAll(".tabs__item");
-// const langBtn = document.querySelectorAll(".header__lang-btn");
-// const contactBtn = document.querySelector("#contact-btn");
-// const contactForm = document.querySelector("#contact-form");
+const mainPageCarouselSettings = {
+  ...defaultCarouselSettings,
+  gap: 60,
+  breakpoints: {
+    1023: {
+      peek: 60,
+    },
+    767: {
+      peek: 30,
+      gap: 30,
+    },
+  },
+};
 
-// burgerBtn.addEventListener("click", () => {
-//   menu.classList.toggle("active");
-// });
+const aboutPageCarouselSettings = {
+  ...defaultCarouselSettings,
+  perView: 4,
+  gap: 30,
+  breakpoints: {
+    767: {
+      perView: 1,
+    },
+  },
+};
 
-// closeBtn.addEventListener("click", () => {
-//   menu.classList.remove("active");
-// });
+const disableBtn = (btn) => {
+  btn.classList.add("main-button--loader");
+  btn.disabled = true;
+};
 
-// tabsItems.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     tabsItems.forEach((el) => el.classList.remove("tabs__item--active"));
-//     item.classList.add("tabs__item--active");
-//   });
-// });
+const enableBtn = (btn) => {
+  btn.classList.remove("main-button--loader");
+  btn.disabled = false;
+};
 
-// langBtn.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     langBtn.forEach((el) => el.classList.remove("header__lang-btn--active"));
-//     item.classList.add("header__lang-btn--active");
-//   });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggleElements = document.querySelectorAll("[data-menu-toggle]");
+  const menuCloseElements = document.querySelectorAll("[data-menu-close]");
+  const menu = document.querySelector(".header__nav");
+  const langBtn = document.querySelectorAll(".header__lang-btn");
+  const contactForms = document.querySelectorAll(".contact__form");
+  const tabsItems = document.querySelectorAll(".tabs__item");
+  const mainPageCarouselElement = document.querySelector(".main-page-carousel");
+  const aboutPageCarouselElement = document.querySelector(
+    ".about-page-carousel"
+  );
 
-// if (contactForm) {
-//   contactForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     disableBtn(contactBtn);
-//     setTimeout(() => {
-//       enableBtn(contactBtn);
-//     }, 2000);
-//   });
-// }
+  const mainPageCarousel = new Glide(
+    ".main-page-carousel",
+    mainPageCarouselSettings
+  );
 
-// function disableBtn(btn) {
-//   btn.classList.add("main-button--loader");
-//   btn.disabled = true;
-// }
+  const aboutPageCarousel = new Glide(
+    ".about-page-carousel",
+    aboutPageCarouselSettings
+  );
 
-// function enableBtn(btn) {
-//   btn.classList.remove("main-button--loader");
-//   btn.disabled = false;
-// }
+  if (mainPageCarouselElement) {
+    mainPageCarousel.mount();
+  }
+
+  if (aboutPageCarouselElement) {
+    aboutPageCarousel.mount();
+  }
+
+  menuToggleElements.forEach((menuToggle) => {
+    menuToggle.addEventListener("click", () => {
+      if (menu) {
+        menu.classList.toggle("active");
+      }
+    });
+  });
+
+  menuCloseElements.forEach((menuToggle) => {
+    menuToggle.addEventListener("click", () => {
+      if (menu) {
+        menu.classList.remove("active");
+      }
+    });
+  });
+
+  contactForms.forEach((form) => {
+    const submitButton = form.querySelector("[type='submit']");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      if (submitButton) {
+        disableBtn(submitButton);
+        setTimeout(() => {
+          enableBtn(submitButton);
+        }, 2000);
+      }
+    });
+  });
+
+  langBtn.forEach((item) => {
+    item.addEventListener("click", () => {
+      langBtn.forEach((el) => el.classList.remove("header__lang-btn--active"));
+      item.classList.add("header__lang-btn--active");
+    });
+  });
+
+  tabsItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      tabsItems.forEach((el) => el.classList.remove("tabs__item--active"));
+      item.classList.add("tabs__item--active");
+    });
+  });
+});
